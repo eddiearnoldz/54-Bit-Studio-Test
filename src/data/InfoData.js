4e8638703231642c2b4678ee2d3d8a24e5a77b56
eddiearnoldz <eddiearnoldz@me.com>
2022-05-06 09:56:19 +0100

    readme updated

---
+++ src/data/InfoData.js
@@ -0,0 +1,21 @@
+import ImageOne from '../images/michael-effendy-xdGigMgUwaQ-unsplash (1).jpg'
+import ImageTwo from '../images/sonnie-hiles-iANAdaNu7mg-unsplash.jpg'
+
+export const InfoData = {
+  heading: 'Explore our beautiful homes',
+  paragraphOne: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique nesciunt nemo laborum autem non magnam delectus recusandae. ',
+  paragraphTwo: 'Mollitia dolor possimus sed quod, vitae, eveniet ex non rem, accusantium sunt voluptate.',
+  buttonLabel: 'View Homes',
+  image: ImageOne,
+  reverse: false,
+  delay: 100
+} 
+export const InfoDataTwo = {
+  heading: 'Something Fancy',
+  paragraphOne: 'Mollitia dolor possimus sed quod, vitae, eveniet ex non rem, accusantium sunt voluptate.',
+  paragraphTwo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique nesciunt nemo laborum autem non magnam delectus recusandae. ',
+  buttonLabel: 'View Homes',
+  image: ImageTwo,
+  reverse: true,
+  delay: 300
+} 
---
+++ src/data/SliderData.js
@@ -0,0 +1,39 @@
+import ImageOne from '../images/ImageOne.jpg'
+import ImageTwo from '../images/ImageTwo.jpg'
+import ImageThree from '../images/ImageThree.jpg'
+import ImageFour from '../images/ImageFour.jpg'
+
+export const SliderData = [
+  {
+    title: 'Paradise uncovered in Bali, Indonesia',
+    price: '$220 a night',
+    path: '/homes',
+    label: 'View Home',
+    image: ImageOne,
+    alt:'House'
+  },
+  {
+    title: 'Tree house in Austin, Texas',
+    price: '$180 a night',
+    path: '/homes',
+    label: 'View Home',
+    image: ImageTwo,
+    alt:'House'
+  },
+  {
+    title: 'WFH vibes in Alberta, Canada',
+    price: '$300 a night',
+    path: '/homes',
+    label: 'View Home',
+    image: ImageThree,
+    alt:'House'
+  },
+  {
+    title: 'Treehouse in Bondai, Australia',
+    price: '$120 a night',
+    path: '/homes',
+    label: 'View Home',
+    image: ImageFour,
+    alt:'House'
+  }
+]
